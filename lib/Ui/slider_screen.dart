9d955a3c4daf580267ec73e79af66bf179496193
Basit ali <basitalyshah51214@gmail.com>
2025-01-01 06:33:24 -0800

    Enhance SliderScreen layout: add notification switch and slider UI elements

--- lib/Ui/slider_screen.dart
+++ lib/Ui/slider_screen.dart
@@ -11,8 +11,31 @@ class _SliderScreenState extends State<SliderScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: const Text('Slider Example'),
-    ));
+      appBar: AppBar(
+        title: const Text('Slider Example'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          spacing: 12,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text("Notifications"),
+                Switch(value: false, onChanged: (value) {}),
+              ],
+            ),
+            Container(
+              height: 200,
+              color: Colors.red.withValues(alpha: .2),
+            ),
+            Slider(value: .2, onChanged: (value) {})
+          ],
+        ),
+      ),
+    );
   }
 }
